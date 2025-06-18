0a6ed59fb1058d3b401e9950a1074be2828b2d8d
Utku Halis <personal@utkuhalis.com.tr>
2025-06-18 11:14:30 +0300

    wip

---
+++ kodla_multi_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Multi Report',
+    'version': '18.0',
+    'category': 'Account',
+    'license': 'OPL-1',
+    'summary': 'Print invoices double A5 format in A4.',
+    'description': "",
+    'price': 49.99,
+    'currency': 'EUR',
+    'author': 'Kodla Software',
+    'website': 'https://kodla.io',
+    'depends': ['base','account'],
+    'data': [
+            'report/report_multi.xml',
+            'report/report_multi_view.xml',
+    ],
+    "images":["static/description/market.gif"],
+}
